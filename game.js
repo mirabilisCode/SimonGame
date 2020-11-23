1b909ed571e51ac3b3bd9b67ec573d822efdbb2b
Michał Wójcik <michalwojcik@macbook-pro-micha.home>
2020-11-23 13:12:48 +0100

    New game

---
+++ game.js
@@ -0,0 +1,73 @@
+﻿let buttonColours = ["red", "blue", "green", "yellow"];
+let gamePattern = [];
+let userClickedPattern = [];
+let level = 0;
+let started = false;
+
+$(document).keydown(() => {
+    if (started === false) {
+        $("#level-title").text("Level " + level);
+        nextSequence();
+        started = true;
+    }
+});
+
+$(".btn").click((event) => {
+    let userChosenColour = event.target.id;
+    userClickedPattern.push(userChosenColour);
+    playSound(userChosenColour);
+    animatePress(userChosenColour);
+    checkAnswer(userClickedPattern.length - 1);
+})
+
+function nextSequence() {
+    level++;
+    userClickedPattern = [];
+    let randomNumber = Math.floor(Math.random() * 4);
+    let randomChosenColour = buttonColours[randomNumber];
+    gamePattern.push(randomChosenColour);
+
+    setTimeout( () => {
+        $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
+        var audio = new Audio("sounds/" + randomChosenColour + ".mp3");
+        audio.play();}, 350);
+    $("#level-title").text("Level " + level);
+}
+
+function playSound(name) {
+    var audio = new Audio("sounds/" + name + ".mp3");
+    audio.play();
+}
+
+function animatePress(currentColour) {
+    let target = "." + currentColour;
+    $(target).addClass("pressed");
+    setTimeout(() => {
+        $(target).removeClass("pressed");
+    }, 100);
+}
+
+function checkAnswer(currentLevel) {
+    if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
+        if (gamePattern.length === userClickedPattern.length) {
+            setTimeout(() => {
+                nextSequence();
+            }, 1000);
+        }
+    } else {
+        let audio2 = new Audio("sounds/wrong.mp3");
+        audio2.play();
+        $("body").addClass("game-over");
+        setTimeout( () => {
+            $("body").removeClass("game-over");
+        });
+        $("#level-title").text("Game Over, Press Any Key to Restart");
+        startOver();
+    }
+}
+
+function startOver() {
+    level = 0;
+    started = false;
+    gamePattern = [];
+}
